c1ff0f3897b5d88ac77655102b551ddaaa57accb
Hans Becker <hansbecker91@gmail.com>
2016-04-25 17:22:35 +0200

    start.sh now searches for Eclipse and Gradle jar Updated command line parameter in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,26 @@
-java -jar dist/services-java.jar \
-    -t -p -o -c \
-    -address tcp://* \
-    -registration tcp://*:5004 \
-    -configuration tcp://*:5007 \
-    -dyndeps tcp://*:5009 \
-    -resources 5050
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+eclipse_jar="$DIR/dist/services-java.jar"
+gradle_jar="$DIR/build/libs/services-java.jar"
+
+if [ -f "$eclipse_jar" ]; then
+    jar="$eclipse_jar"
+elif [ -f "$gradle_jar" ]; then
+    jar="$gradle_jar"
+else
+    printf "No jar found. Please build the project first.\n" >&2
+    exit 99
+fi
+
+java -jar "$jar" \
+     -tokenizer \
+     -javaccparser \
+     -outline \
+     -codecompletion \
+     -address tcp://* \
+     -registration tcp://*:5004 \
+     -configuration tcp://*:5007 \
+     -dyndeps tcp://*:5009 \
+     -resources 5050
