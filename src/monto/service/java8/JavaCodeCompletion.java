5a062899221da6ce721344f3593183944851f6af
Hans Becker <hansbecker91@gmail.com>
2016-05-06 14:23:13 +0200

    JavaCodeCompletion now uses JavaCC parser Functionality was reduced greatly (current selection in not taken into account)

--- src/monto/service/java8/JavaCodeCompletion.java
+++ src/monto/service/java8/JavaCodeCompletion.java
@@ -1,18 +1,8 @@
 package monto.service.java8;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import monto.service.MontoService;
 import monto.service.ZMQConfiguration;
-import monto.service.ast.AST;
-import monto.service.ast.ASTVisitor;
-import monto.service.ast.ASTs;
-import monto.service.ast.NonTerminal;
-import monto.service.ast.Terminal;
+import monto.service.ast.*;
 import monto.service.completion.Completion;
 import monto.service.completion.Completions;
 import monto.service.product.ProductMessage;
@@ -25,69 +15,72 @@ import monto.service.source.SourceMessage;
 import monto.service.types.Languages;
 import monto.service.types.ParseException;
 import monto.service.types.Selection;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JavaCodeCompletion extends MontoService {
 
     public JavaCodeCompletion(ZMQConfiguration zmqConfig) {
         super(zmqConfig,
-        		JavaServices.JAVA_CODE_COMPLETION,
-        		"Code Completion",
-        		"A code completion service for Java",
-        		Languages.JAVA,
-        		Products.COMPLETIONS,
-        		options(),
-        		dependencies(
-        				new SourceDependency(Languages.JAVA),
-        				new ProductDependency(JavaServices.JAVA_ANTLR_PARSER, Products.AST, Languages.JAVA)
-        		));
+                JavaServices.JAVA_CODE_COMPLETION,
+                "Code Completion",
+                "A code completion service for Java",
+                Languages.JAVA,
+                Products.COMPLETIONS,
+                options(),
+                dependencies(
+                        new SourceDependency(Languages.JAVA),
+                        new ProductDependency(JavaServices.JAVA_JAVACC_PARSER, Products.AST, Languages.JAVA)
+                ));
     }
 
     @Override
     public ProductMessage onRequest(Request request) throws IOException, ParseException {
-    	SourceMessage version = request.getSourceMessage()
-    			.orElseThrow(() -> new IllegalArgumentException("No version message in request"));
-        ProductMessage ast = request.getProductMessage(Products.AST, Languages.JAVA)
-        		.orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
-        
-        if (version.getSelection().isPresent()) {
-            AST root = ASTs.decode(ast);
-            List<Completion> allcompletions = allCompletions(version.getContent(), root);
-            List<AST> selectedPath = selectedPath(root, version.getSelection().get());
+        long start = System.nanoTime();
 
-            if (selectedPath.size() > 0 && last(selectedPath) instanceof Terminal) {
-                String toBeCompleted = extract(version.getContent(),last(selectedPath));
-                List<Completion> relevant =
-                        allcompletions
-                                .stream()
-                                .filter(comp -> comp.getReplacement().startsWith(toBeCompleted))
-                                .map(comp -> new Completion(
-                                        comp.getDescription() + ": " + comp.getReplacement(),
-                                        comp.getReplacement(),
-                                        version.getSelection().get().getStartOffset(),
-                                        null))
-                                .collect(Collectors.toList());
-                
-                System.out.printf("Relevant: %s\n", relevant);
-                
-                return productMessage(
-                        version.getId(),
-                        version.getSource(),
-                        Products.COMPLETIONS,
-                        Languages.JAVA,
-                        Completions.encode(relevant));
-            }
-            throw new IllegalArgumentException(String.format("Last token in selection path is not a terminal: %s", selectedPath));
-        }
-        throw new IllegalArgumentException("Code completion needs selection");
+        SourceMessage version = request.getSourceMessage()
+                .orElseThrow(() -> new IllegalArgumentException("No version message in request"));
+        ProductMessage ast = request.getProductMessage(Products.AST, Languages.JAVA)
+                .orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
+
+        ASTNode root = ASTs.decodeASTNode((JSONObject) ast.getContents());
+
+        List<Completion> allcompletions = allCompletions(version.getContent(), root);
+        List<Completion> relevant =
+                allcompletions
+                        .stream()
+                        //.filter(comp -> comp.getReplacement().startsWith(toBeCompleted))
+                        .map(comp -> new Completion(
+                                comp.getDescription() + ": " + comp.getReplacement(),
+                                comp.getReplacement(),
+                                3, //version.getSelection().get().getStartOffset(),
+                                null))
+                        .collect(Collectors.toList());
+
+        System.out.printf("Relevant: %s\n", relevant);
+        long end = System.nanoTime();
+
+        return productMessage(
+                version.getId(),
+                version.getSource(),
+                Products.COMPLETIONS,
+                Languages.JAVA,
+                Completions.encode(relevant),
+                ast.getTime() + end - start);
     }
 
-    private List<Completion> allCompletions(String contents, AST root) {
+    private List<Completion> allCompletions(String contents, ASTNode root) {
         AllCompletions completionVisitor = new AllCompletions(contents);
         root.accept(completionVisitor);
         return completionVisitor.getCompletions();
     }
 
-    private class AllCompletions implements ASTVisitor {
+    private class AllCompletions implements ASTNodeVisitor {
 
         private List<Completion> completions = new ArrayList<>();
         private String content;
@@ -98,63 +91,49 @@ public class JavaCodeCompletion extends MontoService {
         }
 
         @Override
-        public void visit(NonTerminal node) {
+        public void visit(ASTNode node) {
             switch (node.getName()) {
 
-                case "packageDeclaration":
-                    AST packageIdentifier = node.getChildren().get(1);
+                case "PackageDeclaration":
                     completions.add(new Completion(
                             "package",
-                            extract(content,packageIdentifier),
+                            extract(content, node),
                             getResource("package.png")));
                     break;
 
-                case "normalClassDeclaration":
-                    structureDeclaration(node, "class", getResource("class.png"));
+                case "ClassDeclaration":
+                    completions.add(new Completion("class", extract(content, node.getChild(1)), getResource("class-public.png")));
+                    traverseChildren(node);
                     break;
 
-                case "fieldDeclaration":
+                case "FieldDeclaration":
                     fieldDeclaration = true;
-                    node.getChildren().forEach(child -> child.accept(this));
+                    traverseChildren(node);
                     fieldDeclaration = false;
-
-                case "variableDeclaratorId":
-                    if (fieldDeclaration)
-                        leaf(node, "field", getResource("private.png"));
                     break;
 
-                case "methodDeclarator":
-                    leaf(node, "method", getResource("public.png"));
+                case "VariableDeclaratorId":
+                    if (fieldDeclaration)
+                        completions.add(new Completion("field", extract(content, node), getResource("field-public.png")));
+                    else
+                        completions.add(new Completion("var", extract(content, node), getResource("field-default.png")));
+                    break;
+
+                case "MethodDeclaration":
+                    completions.add(new Completion("method", extract(content, node.getChild(2)), getResource("method-public.png")));
+                    traverseChildren(node);
+                    break;
 
                 default:
-                    node.getChildren().forEach(child -> child.accept(this));
+                    traverseChildren(node);
             }
         }
 
-        @Override
-        public void visit(Terminal token) {
-
+        private void traverseChildren(ASTNode node) {
+            if (node.getChildren().size() > 0) {
+                node.getChildren().forEach(child -> child.accept(this));
+            }
         }
-
-        private void structureDeclaration(NonTerminal node, String name, URL icon) {
-            Terminal structureIdent = (Terminal) node
-                    .getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .reduce((previous, current) -> current).get();
-            completions.add(new Completion(name, extract(content,structureIdent), icon));
-            node.getChildren().forEach(child -> child.accept(this));
-        }
-
-        private void leaf(NonTerminal node, String name, URL url) {
-            AST ident = node
-                    .getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .findFirst().get();
-            completions.add(new Completion(name, extract(content,ident), url));
-        }
-
 
         public List<Completion> getCompletions() {
             return completions;
@@ -205,8 +184,8 @@ public class JavaCodeCompletion extends MontoService {
         }
     }
 
-    private static String extract(String str, AST indent) {
-	return str.subSequence(indent.getStartOffset(), indent.getStartOffset()+indent.getLength()).toString();
+    private static String extract(String str, IRegion indent) {
+        return str.subSequence(indent.getStartOffset(), indent.getStartOffset() + indent.getLength()).toString();
     }
 
     private static <A> A last(List<A> list) {
