f2220c7c01e84d5b6dd70a073d91258a2f9c9663
Hans Becker <hansbecker91@gmail.com>
2016-06-13 16:29:59 +0200

    Simplified start.sh by removing check for Eclipse jar

--- start.sh
+++ start.sh
@@ -2,14 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-eclipse_jar="$DIR/dist/services-java.jar"
-gradle_jar="$DIR/main/build/libs/main-all.jar"
+gradle_shadow_jar="$DIR/main/build/libs/main-all.jar"
 
-if [ -f "$eclipse_jar" ]; then
-    jar="$eclipse_jar"
-elif [ -f "$gradle_jar" ]; then
-    jar="$gradle_jar"
-else
+if [ ! -f "$gradle_shadow_jar" ]; then
     printf "No jar found. Please build the project first.\n" >&2
     exit 99
 fi
