fe12c08a73c10498767fc9ebf0bdedd32e3450ef
Hans Becker <hansbecker91@gmail.com>
2016-07-05 19:17:33 +0200

    Completions now respect cursor position in source via SourcePositionCommandMessage

--- shared/src/main/java/monto/service/java8/JavaCodeCompletion.java
+++ shared/src/main/java/monto/service/java8/JavaCodeCompletion.java
@@ -1,23 +1,34 @@
 package monto.service.java8;
 
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import monto.service.MontoService;
 import monto.service.ZMQConfiguration;
+import monto.service.command.CommandMessage;
+import monto.service.command.SourcePositionContent;
 import monto.service.completion.Completion;
 import monto.service.gson.GsonMonto;
 import monto.service.identifier.Identifier;
 import monto.service.product.ProductMessage;
 import monto.service.product.Products;
+import monto.service.region.Region;
 import monto.service.registration.ProductDependency;
 import monto.service.registration.SourceDependency;
 import monto.service.request.Request;
 import monto.service.source.SourceMessage;
 import monto.service.types.Languages;
-
-import java.net.URL;
-import java.util.List;
-import java.util.stream.Collectors;
+import monto.service.types.Source;
 
 public class JavaCodeCompletion extends MontoService {
+
+  private Map<Source, Pair<SourceMessage, List<Identifier>>> identifierCache;
 
   public JavaCodeCompletion(ZMQConfiguration zmqConfig) {
     super(
@@ -32,47 +43,105 @@ public class JavaCodeCompletion extends MontoService {
             new SourceDependency(Languages.JAVA),
             new ProductDependency(
                 JavaServices.IDENTIFIER_FINDER, Products.IDENTIFIER, Languages.JAVA)));
+
+    identifierCache = new HashMap<>();
   }
 
   @Override
   public void onRequest(Request request) {
-    long start = System.nanoTime();
 
+    Source mainSource = request.getSource();
     SourceMessage sourceMessage =
         request
-            .getSourceMessage()
+            .getSourceMessage(mainSource)
             .orElseThrow(() -> new IllegalArgumentException("No source message in request"));
     ProductMessage identifierMessage =
         request
-            .getProductMessage(Products.IDENTIFIER, Languages.JAVA)
+            .getProductMessage(mainSource, Products.IDENTIFIER, Languages.JAVA)
             .orElseThrow(() -> new IllegalArgumentException("No identifier message in request"));
 
     List<Identifier> identifiers = GsonMonto.fromJsonArray(identifierMessage, Identifier[].class);
 
-    List<Completion> relevant =
-        identifiers
-            .stream()
-            //.filter(comp -> comp.getReplacement().startsWith(toBeCompleted))
-            .map(
-                identifier
-                    -> new Completion(
-                        identifier.getIdentifier(),
-                        identifier.getIdentifier(),
-                        //sourceMessage.getSelection().get().getStartOffset(),
-                        identifierTypeToIcon(identifier.getType())))
-            .collect(Collectors.toList());
+    identifierCache.put(mainSource, new ImmutablePair<>(sourceMessage, identifiers));
+  }
 
-    //        System.out.printf("Relevant: %s\n", relevant);
+  @Override
+  public void onCommandMessage(CommandMessage commandMessage) {
+    long start = System.nanoTime();
 
-    long end = System.nanoTime();
+    System.out.println(commandMessage);
+    if (commandMessage.getTag().equals(CommandMessage.TAG_SOURCE_POSITION)) {
+      SourcePositionContent sourcePositionContent = commandMessage.asSourcePosition();
 
-    sendProductMessage(
-        sourceMessage.getId(),
-        sourceMessage.getSource(),
-        Products.COMPLETIONS,
-        Languages.JAVA,
-        GsonMonto.toJsonTree(relevant),
-        identifierMessage.getTime() + end - start);
+      if (identifierCache.containsKey(sourcePositionContent.getSource())) {
+        Pair<SourceMessage, List<Identifier>> sourceMessageListPair =
+            identifierCache.get(sourcePositionContent.getSource());
+
+        SourceMessage sourceMessage = sourceMessageListPair.getLeft();
+        List<Identifier> identifiers = sourceMessageListPair.getRight();
+
+        int cursorPosition = sourcePositionContent.getSelection().getStartOffset();
+        String textBeforeCursor =
+            new Region(0, cursorPosition).extract(sourceMessage.getContents());
+
+        int startOfCurrentWord =
+            NumberUtils.max(
+                0,
+                textBeforeCursor.lastIndexOf(' '),
+                textBeforeCursor.lastIndexOf('\t'),
+                textBeforeCursor.lastIndexOf('.'),
+                textBeforeCursor.lastIndexOf(','),
+                textBeforeCursor.lastIndexOf(':'),
+                textBeforeCursor.lastIndexOf(';'),
+                textBeforeCursor.lastIndexOf('"'),
+                textBeforeCursor.lastIndexOf('='),
+                textBeforeCursor.lastIndexOf('+'),
+                textBeforeCursor.lastIndexOf('-'),
+                textBeforeCursor.lastIndexOf('*'),
+                textBeforeCursor.lastIndexOf('/'),
+                textBeforeCursor.lastIndexOf('%'),
+                textBeforeCursor.lastIndexOf('('),
+                textBeforeCursor.lastIndexOf(')'),
+                textBeforeCursor.lastIndexOf('<'),
+                textBeforeCursor.lastIndexOf('>'),
+                textBeforeCursor.lastIndexOf('{'),
+                textBeforeCursor.lastIndexOf('}'),
+                textBeforeCursor.lastIndexOf('['),
+                textBeforeCursor.lastIndexOf(']'));
+        if (startOfCurrentWord != 0) {
+          // exclude limit characters, except on document start
+          startOfCurrentWord += 1;
+        }
+
+        String toBeCompleted =
+            sourceMessage.getContents().substring(startOfCurrentWord, cursorPosition).trim();
+        System.out.println(toBeCompleted);
+
+        List<Completion> relevant =
+            identifiers
+                .stream()
+                .filter(identifier -> identifier.getIdentifier().startsWith(toBeCompleted))
+                .map(
+                    identifier
+                        -> new Completion(
+                            identifier.getIdentifier(),
+                            identifier.getIdentifier(),
+                            //sourceMessage.getSelection().get().getStartOffset(),
+                            identifierTypeToIcon(identifier.getType())))
+                .collect(Collectors.toList());
+
+        System.out.printf("Relevant: %s\n", relevant);
+
+        long end = System.nanoTime();
+        sendProductMessage(
+            sourceMessage.getId(),
+            sourceMessage.getSource(),
+            Products.COMPLETIONS,
+            Languages.JAVA,
+            GsonMonto.toJsonTree(relevant),
+            end - start);
+      }
+    }
   }
 
   private URL identifierTypeToIcon(String identifierType) {
